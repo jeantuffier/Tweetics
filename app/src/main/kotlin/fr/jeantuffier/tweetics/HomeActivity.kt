cbb1fe78dc26234d199d501e7e7198ba3d8b0644
Jean <jean.tuffier@gmail.com>
2018-02-21 09:14:24 +0100

    init repo

---
+++ app/src/main/kotlin/fr/jeantuffier/tweetics/HomeActivity.kt
@@ -0,0 +1,12 @@
+package fr.jeantuffier.tweetics
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class HomeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.home_activity)
+    }
+}
